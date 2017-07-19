0f492b63859b8d7d03c078ada6a08aeca2c92ec5
David Yang <daworld.ny@gmail.com>
2017-07-19 23:37:17 +0500

    Initial implementation

--- src/Prime/PrimeGenerator.php
+++ src/Prime/PrimeGenerator.php
@@ -18,7 +18,12 @@ final class PrimeGenerator
      * 
      * @var array
      */
-    private $options = array();
+    private $options = array(
+        'Version' => '1.0.0',
+        'SignatureMethod' => 'SHA1',
+        'PurchaseCurrency' => '840',
+        'PurchaseCurrencyExponent' => '2'
+    );
 
     /**
      * State initialization
@@ -28,6 +33,41 @@ final class PrimeGenerator
      */
     public function __construct(array $options)
     {
-        $this->options = $options;
+        $this->options = array_merge($this->options, $options);
+    }
+
+    /**
+     * Creates unique order ID
+     * 
+     * @return string
+     */
+    private function createOrderId()
+    {
+        static $id = null;
+
+        // Generate only once for multiple calls
+        if ($id === null) {
+            $id = uniqid();
+        }
+
+        return $id;
+    }
+
+    /**
+     * Creates the signature
+     * 
+     * @return string
+     */
+    public function createSignature()
+    {
+        // Secret string
+        $secret = $this->options['Password'] . 
+                  $this->options['MerID'] . 
+                  $this->options['AcqID'] . 
+                  $this->createOrderId() . 
+                  $this->options['PurchaseAmt'] . 
+                  $this->options['PurchaseCurrency'];
+
+        return base64_encode(sha1($secret, true));
     }
 }
